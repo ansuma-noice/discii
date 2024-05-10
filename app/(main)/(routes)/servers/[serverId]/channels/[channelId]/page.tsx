384b168ff90a89dc48ffd1badec500d7d5d4ca5a
ansuma-noice <ansumahaptra123@gmail.com>
2024-05-11 02:46:43 +0530

    7

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,46 +1,57 @@
 import ChatHeader from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-interface ChannelIdPageProps{
-    params:{
-        serverId:string,
-        channelId:string,
+interface ChannelIdPageProps {
+    params: {
+        serverId: string,
+        channelId: string,
     }
 }
 
 
-const ChannelIdPage=async({
+const ChannelIdPage = async ({
     params
-}:ChannelIdPageProps)=>{
-    const profile=await currentProfile()
+}: ChannelIdPageProps) => {
+    const profile = await currentProfile()
 
-    if(!profile) return redirectToSignIn()
+    if (!profile) return redirectToSignIn()
 
-    const channel=await db.channel.findUnique({
-        where:{
-            id:params.channelId,
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
         }
     })
-    const member=await db.member.findFirst({
-        where:{
-            serverId:params.serverId,
-            profileId:profile.id,
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
         }
     })
 
-    if(!channel || !member)redirect('/')
+    if (!channel || !member) redirect('/')
 
-    return(
-        <div 
-        className="bg-white dark:bg-[#313338] flex flex-col h-full"
+    return (
+        <div
+            className="bg-white dark:bg-[#313338] flex flex-col h-full"
         >
             <ChatHeader
-            name={channel.name}
-            serverId={channel.serverId}
-            type="channel"
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+            <div>Future Messages</div>
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
             />
         </div>
     )
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,33 +1,47 @@
 import { Hash, Menu } from "lucide-react";
+import { MobileToggle } from "../mobile-toggle";
+import { UserAvatar } from "../user-avatar";
+import { SocketIndicator } from "../socket.indicator";
 
-interface ChatHeaderProps{
-    serverId:string;
-    name:string;
-    type:"channel" | "conversation";
-    imageUrl?:string
+interface ChatHeaderProps {
+    serverId: string;
+    name: string;
+    type: "channel" | "conversation";
+    imageUrl?: string
 }
 
 const ChatHeader = ({
-serverId,
-name,
-type,
-imageUrl,
-}:ChatHeaderProps) => {
-    return (  
+    serverId,
+    name,
+    type,
+    imageUrl,
+}: ChatHeaderProps) => {
+    return (
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200
         dark:border-neutral-800 border-b-2">
-            <Menu/>
-            {type==="channel" && (
+            <MobileToggle serverId={serverId} />
+            {type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 
                 dark:text-zinc-400 mr-2"/>
-               )}
-               <p
-               className="font-semibold text-md text-black dark:text-white"
-               >
+            )}
+            {type === "conversation" && (
+                <UserAvatar
+                    src={imageUrl}
+                    className="h-8 w-8 md:h-8 md:w-8 mr-2"
+                />
+            )}
+            <p
+                className="font-semibold text-md text-black dark:text-white"
+            >
                 {name}
-               </p>
+            </p>
+
+            <div className="ml-auto flex items-center">
+                <SocketIndicator/>
+            </div>
+
         </div>
-     );
+    );
 }
- 
+
 export default ChatHeader;
